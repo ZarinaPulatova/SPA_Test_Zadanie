61b7f53558a8f4fe121bc9b498d2faa0974667c9
Zarina <161328402+ZarinaPulatova@users.noreply.github.com>
2025-06-29 20:23:51 +0300

    Create Pagination.vue

---
+++ Pagination.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <button @click="goToPage(currentPage - 1)" :disabled="currentPage <= 1">Назад</button>
+    <span>Страница {{ currentPage }} из {{ totalPages }}</span>
+    <button @click="goToPage(currentPage + 1)" :disabled="currentPage >= totalPages">Вперед</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    currentPage: {
+      type: Number,
+      required: true,
+    },
+    totalPages: {
+      type: Number,
+      required: true,
+    },
+  },
+  methods: {
+    goToPage(page) {
+      if (page > 0 && page <= this.totalPages) {
+        this.$emit('page-changed', page);
+      }
+    },
+  },
+};
+</script>
